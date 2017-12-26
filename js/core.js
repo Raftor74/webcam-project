63ef50fa09f08d02381baf80110a3929622672be
Raftor74 <raftor74@mail.ru>
2017-12-26 17:07:00 +0500

    initial commit

---
+++ js/core.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Андрей on 26.12.2017.
+ */
+(function() {
+    var video = document.getElementById('video'),
+        vendorUrl = window.URL || window.webkitURL;
+    navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.
+            mozGetUserMedia || navigator.msGetUserMedia;
+    navigator.getMedia({
+        video: true,
+        audio: false
+    }, function(stream) {
+        video.src = vendorUrl.createObjectURL(stream);
+        video.play();
+    }, function(error) {
+        alert('Ошибка! Что-то пошло не так, попробуйте позже.');
+        console.log(error);
+    });
+})();
